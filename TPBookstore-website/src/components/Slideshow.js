ed42721fcfa365c02ef84d10760bf4569b384a1c
Nguyễn Viết Phú <nguyenvietphu2001@gmail.com>
2022-10-25 22:08:33 +0700

    Update SingleProduct, CartScreen

--- TPBookstore-website/src/components/Slideshow.js
+++ TPBookstore-website/src/components/Slideshow.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Fade } from 'react-slideshow-image';
-import 'react-slideshow-image/dist/styles.css';
+import React from "react";
+import { Fade } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
 
 const slideImages = [
   {
-    url: '../images/slide-img1.jpg',
-    caption: 'Slide 1'
+    url: "../images/slide-img1.jpg",
+    caption: "Slide 1"
   },
   {
-    url: '../images/slide-img2.jpg',
-    caption: 'Slide 2'
+    url: "../images/slide-img2.jpg",
+    caption: "Slide 2"
   },
   {
-    url: '../images/slide-img3.jpg',
-    caption: 'Slide 3'
-  },
+    url: "../images/slide-img3.jpg",
+    caption: "Slide 3"
+  }
 ];
 
 const Slideshow = () => {
@@ -22,14 +22,16 @@ const Slideshow = () => {
     <div className="slide-container">
       <Fade>
         {slideImages.map((slideImage, index) => (
-          <div className="each-slide" key={index}>
-            {/* <div style={{ 'backgroundImage': `url(${slideImage.url})` }}>
+          <div className="grid wide">
+            <div className="each-slide" key={index}>
+              {/* <div style={{ 'backgroundImage': `url(${slideImage.url})` }}>
             </div> */}
-            <img src={slideImage.url} alt={slideImage.caption} />
+              <img src={slideImage.url} alt={slideImage.caption} />
+            </div>
           </div>
         ))}
       </Fade>
     </div>
-  )
-}
+  );
+};
 export default Slideshow;
