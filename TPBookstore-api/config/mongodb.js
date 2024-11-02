9baed2b8e39c461e5d2c614590a68a27b3153755
tuan.nk <tuan.nk@geekup.vn>
2024-11-02 17:46:20 +0700

    update mongooes config

--- TPBookstore-api/config/mongodb.js
+++ TPBookstore-api/config/mongodb.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const connectDatabase = async () => {
     try {
+        mongoose.set("strictQuery", true);
         const conn = await mongoose.connect(process.env.MONGO_URL, {});
-        // const conn = await mongoose.connect("mongodb://localhost:27017/TPBookstore", {});
         console.log(`Successfully connected to Mongoose... ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error when connect DB: ${error.message}`);
